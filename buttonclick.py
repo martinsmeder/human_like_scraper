63a2d156d55a53ba0cede89e985f9669c3c7b0bb
martinsmeder <119725728+martinsmeder@users.noreply.github.com>
2026-04-23 15:58:58 +0200

    Added human-like mouse movements

--- buttonclick.py
+++ buttonclick.py
@@ -1,9 +1,21 @@
-from pathlib import Path
+# Page to practice on:
+# https://camoufox.com/tests/buttonclick
 
 from camoufox.sync_api import Camoufox
 
-with Camoufox() as browser:
+with Camoufox(humanize=True) as browser:
     page = browser.new_page()
-    page.goto(Path("buttonclick.html").resolve().as_uri())
-    page.locator("button").last.wait_for()
-    page.locator("button").last.click()
+    page.goto("https://camoufox.com/tests/buttonclick")
+    for _ in range(5):
+        button = page.locator(".button") # create button locator
+        button.wait_for() # wait until a .button element exists and is visible
+        box = button.bounding_box() # get the button's position and size
+
+        page.mouse.move(
+            box["x"] + box["width"] / 2,
+            box["y"] + box["height"] / 2,
+        )
+        page.mouse.click(
+            box["x"] + box["width"] / 2,
+            box["y"] + box["height"] / 2,
+        )
