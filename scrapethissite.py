446b42d4c2dd6d30475d553599a27a8b452cf697
martinsmeder <119725728+martinsmeder@users.noreply.github.com>
2026-04-25 15:05:28 +0200

    Scraped first page

--- scrapethissite.py
+++ scrapethissite.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 from camoufox.sync_api import Camoufox
 
 with Camoufox(humanize=True) as browser:
@@ -13,3 +16,17 @@ with Camoufox(humanize=True) as browser:
 
     page.mouse.move(x, y)
     page.mouse.click(x, y)
+    page.wait_for_url("**/pages/simple/")
+    page.wait_for_load_state("networkidle")
+    page.wait_for_selector(".country")
+
+    countries = page.locator(".country").evaluate_all("""els => els.filter(e => e.querySelector(".country-capital")).map(e => ({
+        name: e.querySelector(".country-name").innerText.trim(),
+        capital: e.querySelector(".country-capital").innerText.trim(),
+        population: Number(e.querySelector(".country-population").innerText),
+        area: Number(e.querySelector(".country-area").innerText)
+    }))""")
+
+    os.makedirs("output", exist_ok=True)
+    json.dump(countries, open("output/countries.json", "w"), indent=2)
+    page.go_back()
